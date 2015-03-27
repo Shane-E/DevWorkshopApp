500359bd49644558cebbdc55850dca599abef0df
Shane Eckenrode <sue2@psu.edu>
2015-03-26 20:13:34 -0400

    Adjusted navbar and css stylings.

--- Pinterest/app/models/pin.rb
+++ Pinterest/app/models/pin.rb
@@ -1,4 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => "300x300", :thumb => "100x100"
+	validates_attachment :image, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
+	attr_accessor :image_file_name
 end
